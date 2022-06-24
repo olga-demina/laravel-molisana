5575bc1d7b3f62b4747ce22dd23aac4ea2c71c8c
olga_demina <olga.demina1410@gmail.com>
2022-06-24 12:26:27 +0200

    Header done

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,18 @@
 <header>
-    Header
+    <div class="logo">
+        <img src="{{ asset('images/marchio-sito-test.png') }}" alt="">
+    </div>
+    <nav>
+        <ul class="menu">
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li class="active">
+                <a href="">Prodotti</a>
+            </li>
+            <li>
+                <a href="">News</a>
+            </li>
+        </ul>
+    </nav>
 </header>
